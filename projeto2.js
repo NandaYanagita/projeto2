9d2050c90abbb33f1ef30741e69e96fd3fc614fb
Fernanda Yanagita <144120332+NandaYanagita@users.noreply.github.com>
2023-09-15 10:28:32 -0300

    Projeto final

---
+++ projeto2.js
@@ -0,0 +1,23 @@
+let playerName= "HaruHime"
+console.log("Seja bem vindo " + playerName)
+let numeroPartidas = 100
+let derrota = 13
+const vitoria = numeroPartidas - derrota
+
+if (vitoria <= 10) {
+    nivelPlayer = "Ferro";} 
+else if (vitoria >= 11 && vitoria <= 20) {
+    nivelPlayer = "Bronze";} 
+else if (vitoria >= 21 && vitoria <= 50) {
+    nivelPlayer = "Prata";} 
+else if (vitoria >= 51 && vitoria <= 80) {
+    nivelPlayer = "Ouro";} 
+else if (vitoria >= 81 && vitoria <= 90) {
+    nivelPlayer = "Diamante"} 
+else if (vitoria >= 91 && vitoria <= 100) {
+    nivelPlayer = "Lendário";} 
+else if (vitoria >= 101) {
+    nivelPlayer = "Imortal";} 
+
+console.log (playerName + " em 100 partidas, voçê tem um saldo de " + vitoria + " vitórias e " + derrota + " derrotas.")
+console.log ("Parabéns "+ playerName, "você está no nível " + nivelPlayer + ".")
